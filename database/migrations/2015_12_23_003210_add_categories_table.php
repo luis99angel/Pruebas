a51be6d64c2a48edba7fd8a903ecae3ed8f32213
blog <luis99angel@gmail.com>
2015-12-22 20:59:45 -0600

    creacion de modelos

--- database/migrations/2015_12_23_003210_add_categories_table.php
+++ database/migrations/2015_12_23_003210_add_categories_table.php
@@ -14,6 +14,7 @@ class AddCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name',60);
             $table->timestamps();
         });
     }
